21803b27c73270727d4d3d5ced861703e958bcee
afrizal423 <afrizal2499@gmail.com>
2020-11-18 11:12:26 +0700

    minor pengelompokkan

--- controller/Cf_anemia.go
+++ controller/Cf_anemia.go
@@ -71,14 +71,7 @@ func HitungCF(w http.ResponseWriter, r *http.Request) {
 	// kita unmarshall bracket array dulu
 
 	dec.Token()
-
-	// kita buat perulangan untuk mendapatkan values
-
-	// var dt_penyakit []respon.Penyakit
-	// var dt_rule []respon.Gejala
-	// fmt.Println(len(dt_penyakit))
-	// fmt.Println(dt_rule)
-
+	var dt_json []respon.GetHitung
 	for dec.More() {
 		var GetData respon.GetHitung
 		// decode an array value (Message)
@@ -87,41 +80,54 @@ func HitungCF(w http.ResponseWriter, r *http.Request) {
 			log.Fatal(err)
 		}
 		// log.Print(GetData)
-		fmt.Printf("%v: %v\n", GetData.Kode_gejala, GetData.Persentase_user)
-		rows, err := db.Query("select kode_penyakit, nama_penyakit from spk_anemia_penyakit")
-		iterasi := 0
-		for rows.Next() {
-			pnykt := respon.Penyakit{}
-			if err := rows.Scan(&pnykt.Kode_penyakit, &pnykt.Nama_penyakit); err != nil {
-				log.Fatal(err.Error())
+		dt_json = append(dt_json, GetData)
+		// fmt.Printf("%v: %v\n", GetData.Kode_gejala, GetData.Persentase_user)
+	}
 
-			}
+	// kita buat perulangan untuk mendapatkan values
+	rows, err := db.Query("select kode_penyakit, nama_penyakit from spk_anemia_penyakit")
+	iterasi := 0
+	for rows.Next() {
+		pnykt := respon.Penyakit{}
+		if err := rows.Scan(&pnykt.Kode_penyakit, &pnykt.Nama_penyakit); err != nil {
+			log.Fatal(err.Error())
 
-			// dt_penyakit = append(dt_penyakit, pnykt)
-			sql2 := `select spk_anemia_gejala_penyakit.nama_gejala, spk_anemia_gejala_penyakit.kode_gejala
+		}
+
+		// dt_penyakit = append(dt_penyakit, pnykt)
+		sql2 := `select spk_anemia_gejala_penyakit.nama_gejala, spk_anemia_gejala_penyakit.kode_gejala
 			from spk_anemia_penyakit
 			join spk_anemia_rule on spk_anemia_rule.Penyakitnya_id = spk_anemia_penyakit.id
 			join spk_anemia_gejala_penyakit on spk_anemia_rule.Gejalanya_id = spk_anemia_gejala_penyakit.id
 			where spk_anemia_penyakit.Kode_penyakit=?`
-			rows2, err2 := db.Query(sql2, pnykt.Kode_penyakit)
-			iterasi2 := 0
-			fmt.Println(pnykt.Nama_penyakit)
-			for rows2.Next() {
-				rulenya := respon.Gejala{}
-				err2 = rows2.Scan(&rulenya.Gejala, &rulenya.Kode_gejala)
-				if err2 != nil {
-					log.Panic(err2)
-				}
-				// dt_rule = append(dt_rule, rulenya)
-				// fmt.Println(rulenya.Kode_gejala)
-				if rulenya.Kode_gejala == GetData.Kode_gejala {
-					fmt.Printf("%v: %v = %v\n", GetData.Kode_gejala, GetData.Persentase_user, rulenya.Kode_gejala)
-				}
-				iterasi2++
+		rows2, err2 := db.Query(sql2, pnykt.Kode_penyakit)
+		iterasi2 := 0
+		fmt.Println(pnykt.Nama_penyakit)
+		for rows2.Next() {
+			rulenya := respon.Gejala{}
+			err2 = rows2.Scan(&rulenya.Gejala, &rulenya.Kode_gejala)
+			if err2 != nil {
+				log.Panic(err2)
 			}
-
-			iterasi++
+			// dt_rule = append(dt_rule, rulenya)
+			// fmt.Println(rulenya.Kode_gejala)
+			// if rulenya.Kode_gejala == GetData.Kode_gejala {
+			// 	fmt.Printf("%v: %v = %v\n", GetData.Kode_gejala, GetData.Persentase_user, rulenya.Kode_gejala)
+			// }
+			for _, value := range dt_json {
+				if value.Kode_gejala == rulenya.Kode_gejala {
+					fmt.Printf("- %v = %v\n", value.Kode_gejala, rulenya.Kode_gejala)
+				}
+			}
+			iterasi2++
 		}
+
+		iterasi++
 	}
+
+	// var dt_penyakit []respon.Penyakit
+	// var dt_rule []respon.Gejala
+	// fmt.Println(len(dt_penyakit))
+	fmt.Println(dt_json)
 
 }
